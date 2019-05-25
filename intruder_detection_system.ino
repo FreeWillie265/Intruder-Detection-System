1df884df47845e4a1cdd9a5c89062ad2c4e8e808
willie <wkasakula@gmail.com>
2019-05-25 17:39:47 +0200

    Added code to read PIR values, the send email via blynk if motion is detected

--- intruder_detection_system.ino
+++ intruder_detection_system.ino
@@ -1,9 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
 
+//#define BLYNK_MAX_SENDBYTES 128
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "your blynk auth token";
+int pirPin = D7;
+
+long before = 0;
+
+// Your WiFi credentials.
+char ssid[] = "ssid";
+char pass[] = "password";
+
+
+void sendEmail(){
+  Serial.println("Sending Email!"); // This can be seen in the Serial Monitor
+  Blynk.email("your email address", "Intruder Detected", "An Intruder has been detected in your property!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  pinMode(pirPin, INPUT);   // declare sensor as input
+  Blynk.begin(auth, ssid, pass);
+  before = millis();
+  Serial.print("Initialized!");
+}
 
+void loop()
+{
+  getPirValue();
+  Blynk.run();
+}
+
+void getPirValue(){
+  if(digitalRead(pirPin)){
+    Serial.println("Intruder Detected!"); 
+      /*
+     *send an email 
+     *Make sure no two emails are sent in the same minute
+     */
+    if(millis() - before > 1000 * 15){ //change back to 60s
+      sendEmail();
+      before = millis();  
+    }
+  }
+  delay(500);
 }
